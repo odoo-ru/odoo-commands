7ee0fec5f0cb98ee8b5345801ce7116713385d50
Dmitry Voronin <dimka665@gmail.com>
2023-12-25 20:39:35 +0400

    nox

--- noxfile.py
+++ noxfile.py
@@ -1,12 +1,12 @@
 import nox
 
 # @nox.session(python='3.6')
-# @nox.session(python='3.6', venv_backend='venv', reuse_venv=True)
-@nox.session(python='3.6', venv_backend='venv', reuse_venv=False)
+@nox.session(python='3.6', venv_backend='venv', reuse_venv=True)
+# @nox.session(python='3.6', venv_backend='venv', reuse_venv=False)
 def odoo11(session):
     # session.install('-e', 'odoo-11.0.post20201201/', '--no-deps')
     # session.install('-r', 'odoo11-requirements.txt')
     session.install('-r', 'odoo11-requirements.txt', '--no-deps')
-    session.install('-r', 'req.txt')
+    session.install('-r', 'req.txt', '--no-deps')
 
     session.run('pytest', 'tests')
